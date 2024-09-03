b60b80dc30b8c69536a577b90afa592466a78d27
semjasa83 <marceldezanna@yahoo.de>
2024-09-03 13:45:25 +0200

    rename var

--- models/task.model.js
+++ models/task.model.js
@@ -1,48 +1,48 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: [true, "Please enter Task name"],
-        },
-
-        description: {
-            type: String,
-            required: false,
-        },
-
-        date: {
-            type: Date,
-            required: true,
-            default: 0,
-        },
-
-        priority: {
-            type: String,
-            required: true,
-        },
-
-        category: {
-            type: String,
-            required: false
-        },
-
-        subTasks: {
-            type: [],
-            required: false
-        },
-
-        contacts: {
-            type: [],
-            required: false
-        }
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter Task name"],
     },
-    {
-        timestamps: true,
-    }
+
+    description: {
+      type: String,
+      required: false,
+    },
+
+    dueDate: {
+      type: Date,
+      required: true,
+      default: 0,
+    },
+
+    priority: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: false,
+    },
+
+    subTasks: {
+      type: [],
+      required: false,
+    },
+
+    contacts: {
+      type: [],
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
-const Task = mongoose.model("Product", TaskSchema);
+const Task = mongoose.model("Task", TaskSchema);
 
 module.exports = Task;
